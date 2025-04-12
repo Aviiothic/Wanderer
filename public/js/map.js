84d8db546144c23a19fffebdf42fab46dc1b2eec
i.anupam732 <i.anupam732@gmail.com>
2025-04-12 10:20:30 +0000

    added custom marker

--- public/js/map.js
+++ public/js/map.js
@@ -5,28 +5,38 @@ apna key save krwa lenge aur us variable ks use yahan kr lenge
 maptilersdk.config.apiKey = mapToken;
 const map = new maptilersdk.Map({
   container: "map", // container's id or the HTML element to render the map
-  center: (coordinates), // starting position [lng, lat]
-  zoom: 10, // starting zoom
+  center: coordinates, // starting position [lng, lat]
+  zoom: 12, // starting zoom
 });
 
-// Create a custom home icon
-const homeIcon = document.createElement("div");
-homeIcon.innerHTML = "🏠"; // You can replace this with an image too
-homeIcon.style.fontSize = "20px";
-homeIcon.style.color = "red";
+const markerElement = document.createElement("div");
+markerElement.className = "custom-marker";
+markerElement.innerHTML = `
+  <div class="icon">
+    <svg width="24" height="24" fill="white" viewBox="0 0 24 24">
+      <path d="M12 3l10 9h-3v9h-5v-6h-4v6H5v-9H2z" fill="white"/>
+    </svg>
+  </div>
+`;
 
-// Create the marker with the custom element
-const marker = new maptilersdk.Marker({
-  element: homeIcon,
-  draggable: false
+new maptilersdk.Marker({
+  element: markerElement,
 })
   .setLngLat(coordinates)
+  .addTo(map)
   .setPopup(
     new maptilersdk.Popup({ offset: 25 }).setHTML(`
-      <div style="color: red; font-weight: bold; font-size: 14px;">
-        🏠 The listing is here
-      </div>
-    `)
-  )  
-  .addTo(map);
-  
+    <div class="custom-popup">🏠 The listing is here</div>
+  `)
+  );
+
+new maptilersdk.Marker({
+  element: markerElement,
+})
+  .setLngLat(coordinates)
+  .addTo(map)
+  .setPopup(
+    new maptilersdk.Popup({ offset: 25 }).setHTML(`
+    <div class="custom-popup"><b>🏠 The listing is here</b></div>
+  `)
+  );
